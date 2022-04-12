84038ba3177430c4ba942f4aa0d359fd00bf413e
Chang Wei <changwei1006@gmail.com>
2022-04-13 00:17:23 +0800

    fix variable name

--- udp_hello_client.py
+++ udp_hello_client.py
@@ -1,10 +1,10 @@
 import socket
-addr=('127.0.0.1',10001)
-s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+address = ('127.0.0.1', 10001)
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 for i in range(5):
     data = b'Hello %d' % (i+1)
-    s.sendto(data, addr)
-    recv_data, addr_server = s.recvfrom(2048)
+    s.sendto(data, address)
+    recv_data, address_server = s.recvfrom(2048)
     print(recv_data)
 
--- udp_world_server.py
+++ udp_world_server.py
@@ -1,10 +1,10 @@
 import socket
-addr = ('127.0.0.1',10001)
-s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.bind(addr)
+address = ('127.0.0.1', 10001)
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.bind(address)
 
 while True:
-	data, addr_client = s.recvfrom(2048)
+	data, address_client = s.recvfrom(2048)
 	# if data == b'Hello':
 	print(data)
-	s.sendto(b'World', addr_client)
+	s.sendto(b'World', address_client)
