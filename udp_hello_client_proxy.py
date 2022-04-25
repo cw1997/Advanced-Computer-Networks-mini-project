ba921121a983a232732af5c33db5fb9d9bfa38f1
Chang Wei <changwei1006@gmail.com>
2022-04-25 13:23:19 +0800

    update udp server and client which are over proxy

--- udp_hello_client_proxy.py
+++ udp_hello_client_proxy.py
@@ -1,9 +1,11 @@
 import socket
-address = ('127.0.0.1', 5406)
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_recv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_recv.bind(('127.0.0.1', 5407))
 
 for i in range(50):
     data = b'Hello %d' % (i+1)
-    s.sendto(data, address)
-    recv_data, address_server = s.recvfrom(2048)
-    print(recv_data)
+    sock_send.sendto(data, ('127.0.0.1', 5406))
+    data, address_server = sock_recv.recvfrom(32768)
+    print(address_server, data)
--- udp_world_server_proxy.py
+++ udp_world_server_proxy.py
@@ -1,10 +1,10 @@
 import socket
-address = ('127.0.0.1', 5405)
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(address)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('127.0.0.1', 5405))
 
 while True:
-	data, address_client = s.recvfrom(2048)
+	data, address_client = sock.recvfrom(2048)
 	# if data == b'Hello':
 	print(data)
-	s.sendto(b'World', address_client)
+	sock.sendto(b'World', ('127.0.0.1', 5407))
